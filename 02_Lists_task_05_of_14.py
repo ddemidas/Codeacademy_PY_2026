f73276b66df46b9e2dacaef3e4c5ed97b76dffac
ddemidas <forisoid@gmail.com>
2026-04-06 17:50:37 +0200

    Easter Break coding

---
+++ 02_Lists_task_05_of_14.py
@@ -0,0 +1,24 @@
+'''
+task1
+Jiho works for a gardening store called Petal Power. Jiho keeps a record of orders in a list called orders.
+
+Use print to inspect the orders he has received today.
+'''
+orders = ["daisies", "periwinkle"]
+print(orders)
+'''
+task2
+Jiho just received a new order for "tulips". Use append to add this string to orders.
+'''
+orders.append("tulips")
+print(orders)
+'''
+task3
+Another order has come in! Use append to add "roses" to orders.
+'''
+'''
+task4
+Use print to inspect the orders Jiho has received today.
+'''
+orders.append("roses")
+print(orders)
