0d73a374f4605f27b16877d8ca3deb76be8013c1
Dmytrii Demidas <forisoid@gmail.com>
2026-04-07 16:09:30 +0200

    Introduction to Lists

---
+++ 02_Lists_task_12_of_14.py
@@ -0,0 +1,31 @@
+#Your code below:
+'''
+task1
+We want to have a way to store all of our classroom test score data.
+
+Using the provided table, create a two-dimensional list called class_name_test to represent the data. Each sublist in class_name_test should have one student’s name and their associated score.
+'''
+class_name_test = [["Jenny",90],["Alexus",85.5],["Sam",83],["Ellie",101.5]]
+print(class_name_test)
+'''
+task2
+Use double square brackets ([][]) to select Sam‘s test score from the list class_name_test.
+
+Save it to the variable sams_score.
+
+Print the variable sams_score to see the result.
+'''
+sams_score = class_name_test[2][1]
+print("Sam's score: ")
+print(sams_score)
+'''
+task3
+Use double square brackets ([][]) to select Ellies test score from the list class_name_test. This time only use negative indices!
+
+Save it to the variable ellies_score.
+
+Print the variable ellies_score to see the result.
+'''
+ellies_score = class_name_test[-1][-1]
+print("Ellis scrore: ")
+print(ellies_score)
---
+++ 02_Lists_task_13_of_14.py
@@ -0,0 +1,32 @@
+#Your code below:
+'''
+task1
+Our school is expanding! We are welcoming a new set of students today from all over the world.
+
+Using the provided table, create a two-dimensional list called incoming_class to represent the data. Each sublist in incoming_class should contain the name, nationality, and grade for a single student.
+'''
+'''
+task2
+"Madison" passed an exam to advance a grade. She will be pushed into 8th grade rather than her current 7th in our list.
+
+Modify the list using double brackets [][] to make the change. Use positive indices.
+
+Print incoming_class to see our change.
+'''
+incoming_class = [["Kenny", "American", 9],["Tanya", "Ukrainian", 9],["Madison", "Indian", 7]]
+print("Incoming class students are: ")
+print(incoming_class)
+incoming_class[2][2]=8
+print(incoming_class)
+'''
+task3
+"Kenny" likes to be called by his nickname "Ken". Modify the list using double brackets [][] to accommodate the change but only using negative indices.
+
+Print incoming_class to see our change.
+'''
+
+print(str(incoming_class[-3][-3]))
+
+incoming_class[-3][-3]="Ken"
+print(str(incoming_class[-3][-3]))
+print(incoming_class)
