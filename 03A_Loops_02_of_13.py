7aa0bf4d8247f6be1b56cdc030364c25f329c6ad
ddemidas <forisoid@gmail.com>
2026-04-18 14:57:49 +0200

    Loops

---
+++ 03A_Loops_02_of_13.py
@@ -0,0 +1,15 @@
+# Write 10 print() statements below! 
+'''
+task1
+Using 10 print() statements, print out: "This can be so much easier with loops!".
+'''
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
---
+++ 03A_Loops_05_of_13.py
@@ -0,0 +1,29 @@
+# While Loop Walkthrough
+count = 0
+print("Starting While Loop")
+while count <= 3:
+  # Loop Body
+  # Print if the condition is still true
+  print("Loop Iteration - count <= 3 is still true")
+  # Print the current value of count 
+  print("Count is currently " + str(count))
+  # Increment count
+  count += 1
+  print(" ----- ")
+  print("While Loop ended")
+
+# Your code below: 
+countdown = 10
+print("Starting While Loop")
+while countdown >= 0:
+  # Loop Body
+  # Print if the condition is still true
+  #print("Loop Iteration - countdown >= 0 is still true")
+  # Print the current value of countdown
+  print(countdown)
+  #print("Countdown is currently " + str(countdown))
+  # Decrement countdown
+  countdown += -1
+  #print(" ----- ")
+  #print("While Loop ended")
+print("We have liftoff!")
