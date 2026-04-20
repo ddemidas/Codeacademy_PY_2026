e21d9a9f0416ba65d6aeb3ff873237aa9d3718c4
Dmytrii Demidas <forisoid@gmail.com>
2026-04-20 16:39:54 +0200

    Functions p.1
    Performing tasks from the module dedicated to functions in Python, part 1.

---
+++ 04A_Functions_05_of_13.py
@@ -0,0 +1,31 @@
+# Your code below: 
+'''
+task1
+We are going to help our trip planner users figure out if they should travel today based on the weather. Let’s let our users know we can check the weather for them.
+
+Write a print() statement that will output Checking the weather for you!.
+'''
+print("Checking the weather for you!")
+'''
+task2
+We took a look outside and see a bright sunny day. Write a function called weather_check() that will print a message to our users that it’s a great day to travel! The function should output:
+
+Looks great outside! Enjoy your trip.
+
+Copy to Clipboard
+
+Note: Don’t call your function just yet! We will do that in the next step.
+'''
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+  print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+
+'''
+task3
+Oh no! It looks like some clouds came in and it started raining. Our users shouldn’t go on a trip in the rain. In our weather_check() function add a second print() statement under the first one which prints a warning message for our travelers! It should print:
+'''
+'''
+task4
+Call the function weather_check().
+'''
+weather_check()
